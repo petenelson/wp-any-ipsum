dae1b9280f0103d57849bf0dabadbd4d2811063b
Pete Nelson <pete@petenelson.com>
2015-06-09 14:46:19 -0500

    changed spaces to tabs

--- lib/class-WPAnyIpsumAPI.php
+++ lib/class-WPAnyIpsumAPI.php
@@ -11,94 +11,95 @@ if ( !defined( 'ABSPATH' ) ) wp_die( 'restricted access' );
 
 if ( !class_exists( 'WPAnyIpsumAPI' ) ) {
 
-    class WPAnyIpsumAPI {
+	class WPAnyIpsumAPI {
 
-        public function plugins_loaded() {
+		public function plugins_loaded() {
 			add_action( 'init', array( $this, 'register_rewrites' ) );
-            add_action( 'template_redirect', array( $this, 'template_redirect' ) );
-        }
+			add_action( 'template_redirect', array( $this, 'template_redirect' ) );
+		}
 
-        public function register_rewrites() {
-            $enabled = apply_filters( 'anyipsum-setting-is-enabled', false, 'anyipsum-settings-api', 'api-enabled' );
-            $endpoint = sanitize_key( apply_filters( 'anyipsum-setting-get', 'api', 'anyipsum-settings-api', 'api-endpoint' ) );
-            if ( $enabled && ! empty( $endpoint ) ) {
-                add_rewrite_tag( '%any-ipsum-api-request%', '1' );
-                add_rewrite_rule( $endpoint . '?', 'index.php?any-ipsum-api-request=1', 'top' );
-            }
-        }
-
-
-        public function template_redirect() {
-            global $wp_query;
-            if ( $wp_query->get( 'any-ipsum-api-request' ) === '1' ) {
-                $this->handle_api_request();
-            }
-        }
+		public function register_rewrites() {
+			$enabled = apply_filters( 'anyipsum-setting-is-enabled', false, 'anyipsum-settings-api', 'api-enabled' );
+			$endpoint = sanitize_key( apply_filters( 'anyipsum-setting-get', 'api', 'anyipsum-settings-api', 'api-endpoint' ) );
+			if ( $enabled && ! empty( $endpoint ) ) {
+				add_rewrite_tag( '%any-ipsum-api-request%', '1' );
+				add_rewrite_rule( $endpoint . '?', 'index.php?any-ipsum-api-request=1', 'top' );
+			}
+		}
 
 
-        function handle_api_request() {
-
-            header( 'Access-Control-Allow-Origin: *' );
-            $type      = WPAnyIpsumCore::get_request( 'type' );
-            $callback  = WPAnyIpsumCore::get_request( 'callback' );
-            $format    = WPAnyIpsumCore::get_request( 'format' );
-
-            if ( !in_array($format, array( 'json', 'text', 'html' ) ) ) {
-                $format = 'json';
-            }
-
-            if ( ! empty ( $type ) || ! empty ( $callback ) ) {
-
-                $args = apply_filters( 'anyipsum-parse-request-args', $_SERVER['REQUEST_METHOD'] === 'POST' ? $_REQUEST : $_SERVER['QUERY_STRING'] );
-                $paras = apply_filters( 'anyipsum-generate-filler', $args );
-
-                $content_type = '';
-                $output = '';
-
-                if ( ! empty( $args['callback'] ) ) {
-                    $output_prefix = $args['callback'] . '(';
-                    $output_suffix = ');';
-                } else {
-                    $output_prefix = '';
-                    $output_suffix = '';
-                }
-
-                switch ( $format ) {
-                    case 'html':
-                        $content_type = 'text/html';
-                        foreach ( $paras as $para ) {
-                            $output .= wpautop( $para );
-                        }
-                        break;
-
-                    case 'text':
-                        $content_type = 'text/plain';
-                        $output = implode( "\n\n", $paras );
-                        break;
-
-                    default: // JSON
-                        $content_type = 'application/json';
-                        $output = json_encode( $paras );
-                        break;
-                }
-
-                if ( ! empty( $args['callback'] ) ) {
-                    $content_type = 'application/javascript';
-                }
-
-                header( 'Content-Type: ' . $content_type . '; charset=' . get_bloginfo( 'charset' ) );
-                header( 'Content-Length: ' . strlen( $output_prefix . $output . $output_suffix ) );
-
-                echo $output_prefix . $output . $output_suffix;
-
-                exit;
-
-            }
+		public function template_redirect() {
+			global $wp_query;
+			if ( $wp_query->get( 'any-ipsum-api-request' ) === '1' ) {
+				$this->handle_api_request();
+			}
+		}
 
 
-        } // end handle_api_request
+	    function handle_api_request() {
+
+			header( 'Access-Control-Allow-Origin: *' );
+			$type      = WPAnyIpsumCore::get_request( 'type' );
+			$callback  = WPAnyIpsumCore::get_request( 'callback' );
+			$format    = WPAnyIpsumCore::get_request( 'format' );
+
+			// no need to keep going if there's nothing to do
+			if ( empty( $type ) && empty( $callback ) ) {
+				return;
+			}
+
+			if ( !in_array( $format, array( 'json', 'text', 'html' ) ) ) {
+				$format = 'json';
+			}
+
+			$args = apply_filters( 'anyipsum-parse-request-args', $_SERVER['REQUEST_METHOD'] === 'POST' ? $_REQUEST : $_SERVER['QUERY_STRING'] );
+			$paras = apply_filters( 'anyipsum-generate-filler', $args );
+
+			$content_type = '';
+			$output = '';
+
+			if ( ! empty( $args['callback'] ) ) {
+				$output_prefix = $args['callback'] . '(';
+				$output_suffix = ');';
+			} else {
+				$output_prefix = '';
+				$output_suffix = '';
+			}
+
+			switch ( $format ) {
+				case 'html':
+					$content_type = 'text/html';
+					foreach ( $paras as $para ) {
+						$output .= wpautop( $para );
+					}
+				break;
+
+				case 'text':
+					$content_type = 'text/plain';
+					$output = implode( "\n\n", $paras );
+				break;
+
+				default: // JSON
+					$content_type = 'application/json';
+					$output = json_encode( $paras );
+				break;
+			}
+
+			if ( ! empty( $args['callback'] ) ) {
+				$content_type = 'application/javascript';
+			}
+
+			header( 'Content-Type: ' . $content_type . '; charset=' . get_bloginfo( 'charset' ) );
+			header( 'Content-Length: ' . strlen( $output_prefix . $output . $output_suffix ) );
+
+			echo $output_prefix . $output . $output_suffix;
+
+			exit;
 
 
-    }
+	    } // end handle_api_request
+
+
+	}
 
 }
