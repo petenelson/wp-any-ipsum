6341a26401242a0bd7a606c2aa2c75b9529f119e
Pete Nelson <pete@petenelson.com>
2018-08-11 19:31:22 -0500

    Added test_default_generator_args

--- tests/phpunit/Core_Tests.php
+++ tests/phpunit/Core_Tests.php
@@ -10,6 +10,13 @@ class Core_Tests extends WP_UnitTestCase {
 
 		$this->assertTrue( is_array( $args ) );
 		$this->assertNotEmpty( $args );
+
+		$this->assertEquals( '', $args['type'] );
+		$this->assertEquals( '', $args['sentences'] );
+		$this->assertEquals( 5, $args['paras'] );
+		$this->assertEquals( 5, $args['max-paras'] );
+		$this->assertEquals( '', $args['callback'] );
+		$this->assertFalse( $args['start-with-lorem'] );
 	}
 
 }
